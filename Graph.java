cc4dcf428b2322c2242396366d1a399695c31d1b
mridul.gain() <mridulgain@gmail.com>
2019-03-29 20:35:20 +0530

    mst

--- Graph.java
+++ Graph.java
@@ -29,7 +29,7 @@ public class Graph
         int loc = searchLoc(n.getVertex1());
         if(loc == -1)
         {
-            vertices[vertexCount] = n.getVertex2();
+            vertices[vertexCount] = n.getVertex1();
             connections[vertexCount].add(n);
             vertexCount++;
         }
@@ -48,6 +48,7 @@ public class Graph
         {
             connections[loc].add(n.reverseNode());
         }
+        edgeCount++;
     }
     public LinkedList<Node> getEdgesOf(String vertex)
     {
@@ -88,8 +89,45 @@ public class Graph
             s = s + vertices[i] + "----->" + PrintLinkedList(connections[i]) + "\n";
         return s; 
     }
-    public void PrimsAlgorithm()
-    {
-       // 
-    }
+	public Graph prims(){
+		//init empty tree
+		Graph mst = new Graph(this.vertexCount, this.vertexCount - 1);
+		String[] mstNodes = new String[this.vertexCount];
+		//initialising heap
+		MinHeap minHeap = new MinHeap(this.vertexCount - 1);
+		//adding 1st vertex of the list
+		mstNodes[0] = this.vertices[0];
+		int mstEdgeCount = 0;
+		int mstVertexCount = 1;
+		//add all edges of this vertex in min heap
+		String v = this.vertices[0];
+		LinkedList<Node> l = this.getEdgesOf(v);
+		//System.out.print(l);
+		ListIterator<Node> it = l.listIterator(0);
+		while(it.hasNext()){
+			minHeap.addElement(it.next());
+		}
+		Node tempNode;
+		System.out.print("ok");
+		// while(mstEdgeCount < this.vertexCount - 1){//total |v| - 1 edges in tree
+		// 	//add the min edge 
+		// 	tempNode = minHeap.extractMin();
+		// 	//System.out.print(tempNode);
+		// 	//cycle detection
+		// 	String v2 = tempNode.getVertex2();
+		// 	if(!foundIn(mstNodes, v2)){
+		// 		mst.addNode(tempNode);
+		// 		mstEdgeCount++;
+		// 		mstNodes[mstVertexCount++] = v2;
+		// 	}
+		// }
+		return mst;
+	}
+	private boolean foundIn(String[] mstNodes, String key){
+		for(int i = 0; i < mstNodes.length; i++){
+			if(key.equals(mstNodes[i]))
+				return false;
+		}
+		return true;
+	}
 }
